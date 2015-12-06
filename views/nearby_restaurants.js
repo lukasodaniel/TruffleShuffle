dbdf8f0b5c6c38b3d7f033e24030cba3a447e931
Lukas O'Daniel <lukas.a.odaniel@gmail.com>
2015-12-06 15:16:09 -0500

    restaurant skeleton added

---
+++ views/nearby_restaurants.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    var data = new Array();
+    var restaurants = getRestaurants();
+    out = $.getJSON("getRestaurants", function(jd){
+        for(i in jd){
+            data[i] = jd[i];
+            //console.log(jd[i]);
+            var row = $('<tr></tr>');
+            var name = $('<td>'+data[i]['name']+'</td>');
+            var address = $('<td>'+data[i]['address']+'</td>');
+
+            $("#restaurants").append(row);
+            
+            row.append(name);
+            row.append(address);
+
+
+        }
+
+        /*$("button").click(function() {
+            $.ajax("");
+        });*/
+
+    });
+    
+
+    
+    
+});
+
+
